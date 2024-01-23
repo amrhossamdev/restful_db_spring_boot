b2280fd30db1633df392ea67d969031172772e34
Amr Hossam <amrhossam.dev@gmail.com>
2024-01-24 01:46:29 +0200

    Refactor
    - Added spring rest

--- src/main/java/com/amr/restfuldb/dao/EmployeeRepository.java
+++ src/main/java/com/amr/restfuldb/dao/EmployeeRepository.java
@@ -2,6 +2,10 @@ package com.amr.restfuldb.dao;
 
 import com.amr.restfuldb.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+
+// to change endpoint
+// @RepositoryRestResource(path = "members")
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
 }
